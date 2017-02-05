5a2a7125e143cb35b39eb0abdbf10ca13be53b3e
Charles Hudson Baker <charlesprobaker@gmail.com>
2017-02-05 15:42:25 -0700

    ADD get initial state
    using state for function instead of js variables

--- cow.js
+++ cow.js
@@ -2,26 +2,24 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Button = require('./button.js');
 
-var countNum = (
-                <div id="counter">
-                    {counter}
-                </div>
-               );
-
-var counter = 0;
-
-var ClickCow = React.createClass ({
-    Handleclick: function () {
-        counter ++;
-    },
-
-    render: function () {
-        return <Button onClick={this.handleClick} />;
-    }
+var Talker = React.createClass({
+  getInitialState: function() {
+    return {count: 0};
+  },
+  
+  handleClick: function () {
+    console.log('clicked!');
+    var currentCount = this.state.count;
+    this.setState({count: currentCount + 1});
+  },
+  
+  render: function () {
+    return <Button count={this.state.count}
+    onClick={this.handleClick} />;
+  }
 });
 
 ReactDOM.render(
-    <clickCow />,
-    countNum,
-    document.getElementById('content')
+  <Talker />,
+  document.getElementById('root')
 );
