714fd7e728cd2f42648be513a61ec0a129af6d49
Charles Hudson Baker <charlesprobaker@gmail.com>
2017-02-05 15:48:50 -0700

    ADD props onClick
    adds props to img cow, and this.props.count for div id counter

--- button.js
+++ button.js
@@ -1,13 +1,22 @@
 var React = require('react');
 
+// this.props.onClick handles the function 
+// affecting the cow image holding the prop,
+// used to pass in the click function
 var Button = React.createClass({
   render: function () {
-    return (
-        <div id="cow">
-        <img id="cow_pic" src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Cow_female_black_white.jpg">
+    var html = (
+      <div id="container">
+        <img id="cow" src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Cow_female_black_white.jpg"
+         onClick={this.props.onClick} />
+        <div id="counter">
+          { this.props.count }
         </div>
+      </div>
     );
+    return html;
   }
 });
 
+// exports this component so it can be accessed by cow.js
 module.exports = Button;
